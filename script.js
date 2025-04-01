e42f4e9c5d8d558100a040987100738276f77fc3
motoneo45 <102189233+motoneo45@users.noreply.github.com>
2025-04-01 12:42:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function showInfo(name, job, email) {
+    const infoBox = document.getElementById('infoBox');
+    document.getElementById('infoText').innerHTML = `<strong>${name}</strong><br>${job}<br>${email}`;
+    infoBox.style.display = 'block';
+}
+
+function closeInfo() {
+    document.getElementById('infoBox').style.display = 'none';
+}
